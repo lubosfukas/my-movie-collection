232363480abc496684820af6ec023a02f26a92b4
lubosfukas <49817123+lubosfukas@users.noreply.github.com>
2021-06-24 17:09:24 +0200

    feat: :sparkles: Added infinite scroll
    Replaced "More" button with infinite scroll

--- src/pages/SearchMovies.tsx
+++ src/pages/SearchMovies.tsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useContext } from 'react'
 import { useHistory } from 'react-router-dom'
-import { IconButton } from '@material-ui/core'
+import InfiniteScroll from 'react-infinite-scroll-component'
 import { Alert, AlertTitle } from '@material-ui/lab'
-import ExpandMore from '@material-ui/icons/ExpandMore'
 
-import { Thumbnail, LoadingSpinner } from '../../components'
-import { useFetchMovies } from '../../hooks'
-import { IMovie } from '../../utils/types'
-import './SearchMovies.scss'
-import { MovieContext } from '../../App'
+import { Thumbnail, LoadingSpinner } from '../components'
+import { useFetchMovies } from '../hooks'
+import { IMovie } from '../utils/types'
+import { MovieContext } from '../App'
 
 interface IProps {
     title: string
@@ -26,12 +24,15 @@ const SearchMovies: React.FC<IProps> = ({ title }) => {
         throw new Error('useCount must be used within a CountProvider')
     }
 
-    const { data, isFetching, fetchNextPage, isFetchingNextPage } = useFetchMovies(movieContext.searchTerm)
+    const { data, isFetching, fetchNextPage } = useFetchMovies(movieContext.searchTerm)
 
     const responseError = data && data.pages.length === 1 ? data.pages[0].Error : ''
+
     const moviesArrays =
         data && data.pages.map(response => (response.Search ? response.Search.map(movie => movie) : []))
     const movies = moviesArrays ? moviesArrays.reduce((prev, cur) => prev.concat(cur), []) : []
+
+    const dataLength = movies.length
     const totalResults = data && data.pages[0].totalResults ? parseInt(data.pages[0].totalResults, 10) : 0
 
     const handleOpenDetail = (imdbId: string) => {
@@ -52,31 +53,29 @@ const SearchMovies: React.FC<IProps> = ({ title }) => {
         )
 
     return (
-        <div className="column-flex-box">
-            {isFetching && <LoadingSpinner />}
-            <div className="row-flex-box">
-                {movies.map((movie: IMovie) => {
-                    const imdbId = movie['imdbID']
-                    const title = movie['Title']
+        <InfiniteScroll
+            dataLength={dataLength}
+            next={fetchNextPage}
+            hasMore={!!moreMovies}
+            loader={<LoadingSpinner />}
+            style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}
+        >
+            {movies.map((movie: IMovie) => {
+                const imdbId = movie['imdbID']
+                const title = movie['Title']
 
-                    return (
-                        <Thumbnail
-                            key={`${imdbId}-${title}`}
-                            imdbId={imdbId}
-                            title={title}
-                            year={movie['Year']}
-                            poster={movie['Poster']}
-                            onClicked={(imdbId: string) => handleOpenDetail(imdbId)}
-                        />
-                    )
-                })}
-            </div>
-            {moreMovies && (
-                <IconButton onClick={() => fetchNextPage()} disabled={!moreMovies || isFetchingNextPage}>
-                    <ExpandMore />
-                </IconButton>
-            )}
-        </div>
+                return (
+                    <Thumbnail
+                        key={`${imdbId}-${title}`}
+                        imdbId={imdbId}
+                        title={title}
+                        year={movie['Year']}
+                        poster={movie['Poster']}
+                        onClicked={(imdbId: string) => handleOpenDetail(imdbId)}
+                    />
+                )
+            })}
+        </InfiniteScroll>
     )
 }
 
